0176e79f24e7b61cf95a3731660bfbdccd068d3c
Andy Hu <andy45.hu@gmail.com>
2024-01-20 13:46:46 -0800

    new files

--- NwHacksUI/src/App.jsx
+++ NwHacksUI/src/App.jsx
@@ -3,18 +3,19 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 
 import Home from './components/Home'
-import LoginPage from './auth/LoginPage'
+import LoginPage from './components/LoginPage'
+import PrivateRoute from './auth/PrivateRoute'
 
 function App() {
 
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path='/login' element={<LoginPage />}/>
           <Route path='/' element={<PrivateRoute><Home /></PrivateRoute>} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   )
 }
