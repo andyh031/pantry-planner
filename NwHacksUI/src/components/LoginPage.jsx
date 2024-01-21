7d8abd32a84638b539f79ae833543dbe27ed0cf2
Falconfeather4 <huangsimiao2014@gmail.com>
2024-01-20 16:13:49 -0800

    set up frontend oauth login

--- NwHacksUI/src/components/LoginPage.jsx
+++ NwHacksUI/src/components/LoginPage.jsx
@@ -1,9 +1,29 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 function LoginPage() {
-  return(
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  return (
     <div>
-      Login Page
+      <LoginButton />
+      {isAuthenticated && (
+        <>
+          <h2>Welcome, {user.name}</h2>
+          <p>User ID: {user.sub}</p>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default LoginPage
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+
+
+
