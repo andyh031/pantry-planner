9c0ec06d5ab8711c307cb071e3a0bdab2fdf57c6
Andy Hu <andy45.hu@gmail.com>
2024-01-20 23:53:33 -0800

    tweaks

--- NwHacksUI/src/auth/PrivateRoute.jsx
+++ NwHacksUI/src/auth/PrivateRoute.jsx
@@ -5,6 +5,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 function PrivateRoute({ children }) {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  return isAuthenticated ? children : <Navigate to="/login" />}
+  return isAuthenticated ? children : <Navigate to="/" />}
 
 export default PrivateRoute;
--- NwHacksUI/src/components/LoginPage.jsx
+++ NwHacksUI/src/components/LoginPage.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+// import React from "react";
+// import { useEffect } from "react";
+// import { useAuth0 } from "@auth0/auth0-react";
 
-import { userApi } from "../api/UserApi";
+// import { userApi } from "../api/UserApi";
 
-function LoginPage() {
-  const { user, isAuthenticated, isLoading } = useAuth0();
-  useEffect(() => {
-    if (isAuthenticated) {
-      userApi.createUser(user);
-    }
-  }, [isAuthenticated]);
+// function LoginPage() {
+//   const { user, isAuthenticated, isLoading } = useAuth0();
+//   useEffect(() => {
+//     if (isAuthenticated) {
+//       userApi.createUser(user);
+//     }
+//   }, [isAuthenticated]);
+  
+//   return (
+//     <div>
+//       <LoginButton />
+//       {isAuthenticated && (
+//         <>
+//           <h2>Welcome, {user.name}</h2>
+//           <p>User ID: {user.sub}</p>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
 
-  return (
-    <div>
-      <LoginButton />
-      {isAuthenticated && (
-        <>
-          <h2>Welcome, {user.name}</h2>
-          <p>User ID: {user.sub}</p>
-        </>
-      )}
-    </div>
-  );
-}
+// export default LoginPage
 
-export default LoginPage
-
-const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
-};
+// const LoginButton = () => {
+//   const { loginWithRedirect } = useAuth0();
+//   return <button onClick={() => loginWithRedirect()}>Log In</button>;
+// };
 
 
 
